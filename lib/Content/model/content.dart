c3e9b26b8eb8a6af21183898b2d23aa4b65b1f3e
Sebastian Valencia <61368964+sebasnop@users.noreply.github.com>
2020-05-04 00:03:05 -0500

    file_picker package added. Content class created

---
+++ lib/Content/model/content.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Content {
+
+  String title;
+  String description;
+  String type;
+  String url;
+
+  Content({Key key, @required this.title, this.description, @required this.type, this.url});
+
+}
